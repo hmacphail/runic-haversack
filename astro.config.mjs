5c91c75048b524403fc26aafc4cbf816f53b3b8a
Heather MacPhail <heather@hmacphail.com>
2025-02-17 14:49:59 -0800

    Callout and footer matching to Square site

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
+import icon from "astro-icon";
 import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
@@ -17,7 +18,7 @@ export default defineConfig({
     drafts: true
   },
   site: 'https://lexingtonthemes.com',
-  integrations: [tailwind(), sitemap(), mdx()],
+  integrations: [tailwind(), sitemap(), mdx(), icon()],
   output: "hybrid",
   adapter: vercel()
 });
